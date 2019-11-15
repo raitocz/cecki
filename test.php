ef2dd9beb9466140f1f44ce492617f8e549686ae
raito_cz <martin.jagr@cortex.cz>
2019-11-15 15:56:34 +0100

    Removed test.php file

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-require_once('vendor/autoload.php');
-
-$string = new \RaitoCZ\Cecki\Type\StringType('Hello Razu and Flea! It is really a nice to meet you.');
-dump($string->improveReadability()); // Hallo Rizi ind Flai! At es raillo i neca to maat ooi.
-
-dump($string->improveSlightly());
